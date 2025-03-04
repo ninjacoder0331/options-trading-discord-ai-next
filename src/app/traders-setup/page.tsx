12a1a97ebe6b9c1c786d29a9649bafd10a94aa8f
clbarriosb <christopherhidval2@gmail.com>
2025-03-04 14:04:41 -0800

    feat : Trader Setup

--- src/app/traders-setup/page.tsx
+++ src/app/traders-setup/page.tsx
@@ -13,33 +13,10 @@ import MyTable from "@/components/myTable";
 // };
 
 const TradersSetup = () => {
-  const [traders, setTraders] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  // Method 1: Using .then()
-  useEffect(() => {
-    setIsLoading(true);
-    apiClient.get('/api/trader/getTraders')
-    .then(response => {
-      // Handle success
-      setTraders(response.data);
-      setIsLoading(false);
-      console.log(response.data);
-      
-    })
-    .catch(error => {
-      // Handle error
-      setIsLoading(false);
-      console.error('Error:', error);
-    });
-  }, []);
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  
 
   return (
-      <Suspense fallback={<TopChannelsSkeleton />}>
-        <MyTable traders={traders} />
-      </Suspense>
+    <MyTable/>
   );
 };
 
